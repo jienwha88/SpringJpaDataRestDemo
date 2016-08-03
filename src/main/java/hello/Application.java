74a8e1bc4a9cf59007e1ae1f44f320d772b62f9d
William Wu <william_wu@homedepot.com>
2016-08-02 23:07:59 -0400

    Completed tutorial

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by williamwu on 8/2/16.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main (String [] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
